9cd91865720b2d5ee3849aff2a5791232cd30724
Arunning6 <chenliangliang68@163.com>
2018-04-18 19:03:24 +0800

    Initial commit

---
+++ src/test/com/cll/activiti/ActivitiTest.java
@@ -0,0 +1,17 @@
+package com.cll.activiti;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.junit.Test;
+
+/**
+ * @author chenliangliang
+ * @date 2018/4/18
+ */
+public class ActivitiTest {
+
+    @Test
+    public void test0(){
+        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+    }
+}
